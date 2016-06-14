78f7e8799b22298a47709e9766ee31e7b63c334b
Isman Usoh <isman.isoh@gmail.com>
2016-06-14 22:36:25 +0700

    ปรับปรุง Home คอมโพเน้น

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,27 +1,37 @@
 import {Component} from "@angular/core";
 import {Http} from "@angular/http";
-import {NavController} from 'ionic-angular';
+import {Alert, NavController} from 'ionic-angular';
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
   private values: any;
-  
+
   constructor(private http: Http,
-    private _navController: NavController) {
+    private nav: NavController) {
     http.get("http://jsonplaceholder.typicode.com/posts")
       .map(res => res.json())
       .subscribe(
         data => this.values = data,
-        err => console.log(err),
+        err => this.onError(err),
         () => console.log("Completed")
       );
   }
-
-  /*
-    pushPage(){
-      this._navController.push(SomeImportedPage, { userId: "12345"});
-    }
-  */
+  onError(err: Error) {
+    let alert = Alert.create({
+      title: "Error",
+      subTitle: err.message,
+      buttons: ['OK']
+    });
+    this.nav.present(alert);
+  }
+  onSelect(item) {
+    let alert = Alert.create({
+      title: item.title,
+      subTitle: item.body,
+      buttons: ['OK']
+    });
+    this.nav.present(alert);
+  }
 }
