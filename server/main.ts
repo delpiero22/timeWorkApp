fa0f72386c5756d48d72e9b3467549cbf9d8b37d
Isman Usoh <isman.isoh@gmail.com>
2016-06-18 10:30:34 +0700

    แยก server bootstrap

--- server/main.ts
+++ server/main.ts
@@ -1,33 +1,25 @@
 import * as express from "express";
-import  *as bodyParser from 'body-parser';
-
-const app = express();
+import *as bodyParser from 'body-parser';
 
 const data = require("./services/data.json");
 const user = require("./services/user.json");
 const timeWork = require("./services/timework.json");
 
+const app = express();
 app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
-   res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "*");
     next();
 })
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
-// api/url?id=100
+
 app.get("/", (req, res) => {
-    // const id = req.query.id;
     res.end("Hello World!");
 });
-
-//
 app.get("/TestRESTful/rest/UserService/users", (req, res) => {
-    // const id = req.query.id;
     res.json(user);
 });
-
-// api/url
-// body: id=120,name=Isa
 app.post("/api/timework", (req, res) => {
     res.json(data);
 });
@@ -37,7 +29,7 @@ app.post("/MobileAppRAOT/rest/TimeWorkService/timeWork", (req, res) => {
 
 
 app.listen(8080, (err) => {
-    if(err) {
+    if (err) {
         console.log(err);
         return;
     }
