0051440c5e51c2a4f06c439bbf4902f33d24e9d4
Isman Usoh <isman.isoh@gmail.com>
2016-06-24 06:32:51 +0700

    แยกออกเป็นจากไฟล์ app.html และรองรับ sidebar

--- app/app.ts
+++ app/app.ts
@@ -1,20 +1,89 @@
-import {Component} from '@angular/core';
-import {Platform, ionicBootstrap} from 'ionic-angular';
-import {StatusBar} from 'ionic-native';
-import {HomePage} from './pages/home/home.component';
-import {TimeWorkSearch} from "./pages/timework/timeworkSearch.component";
+import { Component, ViewChild } from "@angular/core";
+import { App, ionicBootstrap, Platform, Nav, Events } from "ionic-angular";
+import { StatusBar } from "ionic-native";
+import { Observable } from "rxjs";
+
+import { Page1 } from "./pages/page1/page1";
+import { Page2 } from "./pages/page2/page2";
+
+import {LoginPage} from "./pages/login/login.component";
+import {HomePage} from "./pages/home/home.component";
+import {TimeWorkSearchPage} from "./pages/timework/timeworkSearch.component";
+
+import {GlobalVars, User} from "./services/globalVars";
+
+interface Page {
+  alias: string;
+  title: string;
+  component: any;
+  icon: string
+}
 
 @Component({
-  template: '<ion-nav [root]="rootPage"></ion-nav>'
+  providers: [GlobalVars],
+  templateUrl: "build/app.html"
 })
-export class MyApp {
-  rootPage: any = TimeWorkSearch;
+class MyApp {
+  @ViewChild(Nav) nav: Nav;
 
-  constructor(platform: Platform) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
+  rootPage: any;
+
+  pages: Array<Page>;
+
+  user: User;
+
+  constructor(private platform: Platform,
+    private events: Events,
+    private globalVars: GlobalVars) {
+    this.initializeApp();
+    this.loadUser();
+    this.loadPages();
+    this.listenToLoginEvents();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
       StatusBar.styleDefault();
+    });
+  }
+
+  loadUser() {
+    this.user = this.globalVars.getUser();
+  }
+
+
+  loadPages() {
+    if (this.globalVars.getUser().auth) {
+      this.rootPage = HomePage;
+      this.pages = [
+        { alias: "home", title: "หน้าหลัก", component: HomePage, icon: "ios-home" },
+        { alias: "timework", title: "เวลางาน", component: TimeWorkSearchPage, icon: "ios-clock" },
+        { alias: "test-1", title: "ทดสอบ 1", component: Page1, icon: "ios-code-working" },
+        { alias: "test-2", title: "ทดสอย 2", component: Page2, icon: "ios-code-working" }
+      ];
+    } else {
+      this.rootPage = LoginPage;
+      this.pages = [
+        { alias: "login", title: "ล็อกอิน", component: LoginPage, icon: "ios-key" }
+      ];
+    }
+  }
+
+  openPage(page: Page) {
+    this.nav.setRoot(page.component);
+  }
+
+  listenToLoginEvents() {
+    this.events.subscribe('user:login', (userDataEvent) => {
+      this.globalVars.userLogin(userDataEvent[0]);
+      this.loadUser();
+      this.loadPages();
+    });
+
+    this.events.subscribe('user:logout', () => {
+      this.globalVars.userLogout();
+      this.loadUser();
+      this.loadPages();
     });
   }
 }
