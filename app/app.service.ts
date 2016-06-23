96842de12e7ebca02f4e7d85802229765044a3a3
Isman Usoh <isman.isoh@gmail.com>
2016-06-24 06:35:45 +0700

    เพิ่ม service login logout

--- app/app.service.ts
+++ app/app.service.ts
@@ -12,34 +12,20 @@ export class AppService {
 
     }
 
+    userLogin(empId: number, password: string): Promise<any> {
+        if(empId === 123456 && password === "123456789") {
+            return Promise.resolve()
+        } else {
+            return Promise.resolve()
+        }
+    }
+ 
+    userLogout(empId: number) {
+        return Promise.resolve(true);
+    }
+
 
     find(empId?: String, password?: String, date?: Date): Promise<TimeWork[]> {
         return Promise.resolve(timeworkDatas);
     }
-
-    // callTimeWork(empId?: String, password?: String, date?: Date): Promise<TimeWork[]> {
-    //     const bodyArr = [];
-    //     if (empId) {
-    //         bodyArr.push("id=" + empId);
-    //     }
-    //     if (password) {
-    //         bodyArr.push("pass=" + password);
-    //     }
-    //     if (date) {
-    //         bodyArr.push("day=" + (date.getDay() + 1));
-    //         bodyArr.push("month=" + (date.getMonth() + 1));
-    //         bodyArr.push("year=" + (date.getFullYear() + 543));
-    //     }
-    //     const body = bodyArr.join("&");
-    //     const headers = new Headers();
-    //     headers.append("Content-Type", "application/x-www-form-urlencoded:charset=utf8");
-
-    //     return this._http
-    //         .post(config.server + "/MobileAppRAOT/rest/TimeWorkService/timeWork", body, { headers: headers })
-    //         .map(res => {
-    //             console.log(res.json());
-    //             return res.json()
-    //         })
-    //         .toPromise()
-    // }
 }
