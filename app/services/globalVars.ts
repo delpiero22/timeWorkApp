84ad9bc93e69de80d7216d76549c7cf0b2ac34a1
Isman Usoh <isman.isoh@gmail.com>
2016-06-24 06:37:40 +0700

    สร้าง service สำหรับแชร์ข้อมูลระหว่าง component

---
+++ app/services/globalVars.ts
@@ -0,0 +1,37 @@
+import {Injectable} from "@angular/core";
+
+export interface User {
+  avatar: string;
+  name: string;
+  email: string;
+  auth?: boolean;
+  cover?: string
+}
+
+@Injectable()
+export class GlobalVars {
+  private user: User;
+
+  constructor() {
+    this.userLogout();
+  }
+
+  getUser() {
+    return this.user;
+  }
+
+  userLogin(user: User) {
+    this.user = user;
+    this.user.auth = true;
+  }
+
+  userLogout() {
+    this.user = {
+      avatar: "https://cdn3.iconfinder.com/data/icons/user-avatars-1/512/users-9-2-128.png",
+      name: "Unknown",
+      email: "กรุณาล็อกอิน",
+      auth: false
+    }
+  }
+
+}
